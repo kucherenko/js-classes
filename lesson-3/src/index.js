a2a3cd7f17b89f2b6b0bda5707f215bd2293f356
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2016-07-18 11:32:52 +0300

    feature(index) angular example

--- lesson-3/src/index.js
+++ lesson-3/src/index.js
@@ -1,1 +1,7 @@
-let a = {};
+import * as angular from 'angular';
+
+angular.module('app', []).controller('MyConroller', ($scope) => {
+  let name = 'Angular';
+  $scope.framework = name;
+  console.log($scope + name);
+});
